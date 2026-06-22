11341fde3a8eee5f508c3d74868074eff9a99044
latikar2810 <latika.r2810@gmail.com>
2026-06-22 12:52:56 +0530

    Add files via upload

---
+++ Project 2/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+let users = [];
+
+// GET API
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+// POST API
+app.post("/users", (req, res) => {
+
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({
+            message: "Name is required"
+        });
+    }
+
+    users.push({ name });
+
+    res.json({
+        message: "User Added Successfully"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
